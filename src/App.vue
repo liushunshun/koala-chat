8183b4e1809328dd88ad78b8ffda20a26790c3de
shunshun.lss <shunshun.lss@alibaba-inc.com>
2019-01-19 21:26:19 +0800

    style

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,70 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="sidebar">
+      <card></card>
+      <list></list>
+    </div>
+
+    <div class="main">
+        <div style="height:40px;line-height:40px;width:100%;text-align:center;">
+              <div style="padding:auto">
+                绘茶
+              </div>
+              <hr style="width:98%;margin:auto;height:1px;border:0px;background-color:#e1e1e1;color:#e1e1e1;"/>
+
+        </div>
+        <message></message>
+        <enter></enter>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Card from './components/card';
+import List from './components/list';
+import Enter from './components/enter';
+import Message from './components/message';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Card,List,Enter,Message
   }
 }
 </script>
 
-<style>
+<style lang="less" scoped>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    margin: 20px auto;
+    width: 800px;
+    height: 600px;
+
+    overflow: hidden;
+    border-radius: 3px;
+
+    .sidebar {
+        float: left;
+        width: 200px;
+        color: #f4f4f4;
+        background-color: #2e3238;
+    }
+    .sidebar, .main {
+        height: 100%;
+    }
+
+    .main {
+        position: relative;
+        overflow: hidden;
+        background-color: #eee;
+    }
+    .text {
+        position: absolute;
+        width: 100%;
+        bottom: 0;
+        left: 0;
+    }
+    .message {
+        height: ~'calc(100% - 160px)';
+    }
 }
 </style>
